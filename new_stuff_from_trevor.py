6d044e5f21aad172c1762650cbb356b07c5acb34
sparke <sparke@mit.edu>
2026-02-13 15:45:24 -0500

    notes

--- new_stuff_from_trevor.py
+++ new_stuff_from_trevor.py
@@ -10,7 +10,7 @@ weight = 5440 * g
 x_to = 45 # m
 
 rho_cruise = 0.909
-C_L_cruise = 0.2
+C_L_cruise = 0.25
 
 S = np.sqrt((weight**2/x_to)*2/(T_c*rho_ground*V_takeoff**2)*1/C_L_to*1/(rho_ground*g))
 print("S:", S)
@@ -23,7 +23,10 @@ V_cruise = np.sqrt((2 * weight) / (rho_cruise * S * C_L_cruise))
 print("V_cruise:", V_cruise)
 
 # Things to do
-# Drag
+# Drag: induced drag + Cd_0
 # Range
     # Consider efficiency (lower than normal plane!)
 # Thrust for climb to make sure it's reasonable
+# Static margin
+# Maybe improve takeoff model to account for acceleration
+# Climb: make sure ROC makes sense
